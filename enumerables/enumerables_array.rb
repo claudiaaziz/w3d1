82c65f93f87b0afff0ef6296d3489a35731b108d
claudiaaziz <claudiahannahh@gmail.com>
2023-09-04 11:37:33 -0400

    figured out zip cannot figure out rotate.

--- enumerables/enumerables_array.rb
+++ enumerables/enumerables_array.rb
@@ -41,33 +41,26 @@ class Array
         flattened
     end
 
-    # def zip(*arrays)
-    #     length = arrays.length
-    #     zipped_result = []
+    def my_zip(*arrays)
+        result = []
 
-    #     (0...length).each do |i|
-    #         ele_at_idx_i = []
-    #         arrays.each do |array| 
-    #             ele_at_idx_i << array[i]
-    #         end
-    #         zipped_result << ele_at_idx_i
-    #     end
+        # loop thr each ele in self
+        self.each_with_index do |ele, i|
+            # start the inner array w the current ele
+            subarray = [ele]
+            
+            # loop thr each array in input arrays
+            arrays.each do |array|
+                # if curr ele at curr array is not nil push ele to subarray else push nil
+                array[i] ? subarray << array[i] : subarray << nil
+            end
 
-    #     zipped_result
-    # end
+            # after the subarray is filled push it to the result. keep looping till self is done
+            result << subarray
+        end
 
-    # def my_zip(*arrays)
-    #     height = self.length
-    #     width = arrays.length
-
-    #     result = Array.new(height) { Array.new(width + 1) }
-
-    #     arrays.inject(self) do |acc, array|
-    #         result = zip(result, array)
-    #     end
-
-    #     result
-    # end
+        result
+    end
 
     # def my_rotate(n = 1)
     #     rotated = self.dup
@@ -78,7 +71,7 @@ class Array
     #             rotated.shift
     #         end
     #     else
-    #         n.times do
+    #         -(n).times do
     #             rotated << self.pop
     #         end
     #     end
@@ -111,21 +104,9 @@ end
 # Write a method my_rotate that returns a new array containing all the elements of the original array in a rotated order. By default, the array should rotate by one element. If a negative value is given, the array is rotated in the opposite direction.
 # Example:
 
-# a = [ "a", "b", "c", "d" ]
-# p a.my_rotate         #=> ["b", "c", "d", "a"]
-# p a.my_rotate(2)      #=> ["c", "d", "a", "b"]
-# a.my_rotate(-3)     #=> ["b", "c", "d", "a"]
-# p a.my_rotate(15)     #=> ["d", "a", "b", "c"]
+a = [ "a", "b", "c", "d" ]
+p a.my_rotate         #=> ["b", "c", "d", "a"]
+p a.my_rotate(2)      #=> ["c", "d", "a", "b"]
+p a.my_rotate(-3)     #=> ["b", "c", "d", "a"]
+p a.my_rotate(15)     #=> ["d", "a", "b", "c"]
 
-
-# Write my_zip to take any number of arguments. It should return a new array containing self.length elements. Each element of the new array should be an array with a length of the input arguments + 1 and contain the merged elements at that index. If the size of any argument is less than self, nil is returned for that location.
-# Example:
-# a = [ 4, 5, 6 ]
-# b = [ 7, 8, 9 ]
-# [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
-# a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
-# [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
-
-# c = [10, 11, 12]
-# d = [13, 14, 15]
-# p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
